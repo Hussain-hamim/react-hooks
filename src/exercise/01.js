5c5202e6e74c58d766fe1a267fab432e292b1993
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-09 10:48:25 +0430

    01 extra credit 1: accept an initialName

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,37 +1,27 @@
 // useState: greeting
-// http://localhost:3000/isolated/exercise/01.js
+// 💯 accept an initialName
+// http://localhost:3000/isolated/final/01.extra-1.js
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  // const name = ''
-  const [name, setName] = React.useState('')
-
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
     setName(event.target.value)
   }
-
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
-      {name ? (
-        <strong>
-          Hello <span style={{color: 'green'}}>{name}</span>
-        </strong>
-      ) : (
-        'Please type your name'
-      )}
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Hussain" />
 }
 
 export default App
