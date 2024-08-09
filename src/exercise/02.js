884caa5b7f9a32797ed325b6d77e534f6c7ffc85
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-09 12:08:15 +0430

    exercise more about local storage

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,12 +8,20 @@ function Greeting({initialName = ''}) {
     window.localStorage.getItem('name') ?? initialName,
   )
 
+  const [lname, setLname] = React.useState(window.localStorage.getItem('lname'))
+
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
+    window.localStorage.setItem('lname', lname)
   })
 
   function handleChange(event) {
     setName(event.target.value)
+    setLname(event.target.value)
+  }
+
+  function handleChange2(event) {
+    setLname(event.target.value)
   }
 
   return (
@@ -21,8 +29,13 @@ function Greeting({initialName = ''}) {
       <form>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
+        <input value={lname} onChange={handleChange2} id="lname" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+
+      <p>
+        value get from local storage: {window.localStorage.getItem('lname')}
+      </p>
     </div>
   )
 }
