00b0772413dc333efc52e9433baa074bd9fc76cd
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-14 11:10:21 +0430

    study pokemon api with form

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,5 +1,69 @@
 import React, {useState, useEffect} from 'react'
 
+function PokemonForm() {
+  const [pokemonName, setPokemonName] = useState('')
+
+  React.useEffect(() => {}, [])
+
+  function handleChange(e) {
+    setPokemonName(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  function handleSelect(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="pokemon-form">
+      <label htmlFor="pokemonName-input">Pokemon Name</label>
+
+      <small>
+        Try{' '}
+        <button
+          className="invisible-button"
+          type="button"
+          onClick={() => handleSelect('pikachu')}
+        >
+          "pikachu"
+        </button>
+        {', '}
+        <button
+          className="invisible-button"
+          type="button"
+          onClick={() => handleSelect('charizard')}
+        >
+          "charizard"
+        </button>
+        {', or '}
+        <button
+          className="invisible-button"
+          type="button"
+          onClick={() => handleSelect('mew')}
+        >
+          "mew"
+        </button>
+      </small>
+      <div>
+        <input
+          className="pokemonName-input"
+          id="pokemonName-input"
+          name="pokemonName"
+          placeholder="Pokemon Name..."
+          value={pokemonName}
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={!pokemonName.length}>
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
+
 function PokemonList() {
   const [pokemons, setPokemons] = useState([])
 
@@ -42,6 +106,9 @@ function PokemonList() {
 
   return (
     <>
+      <div>
+        <PokemonForm />
+      </div>
       {pokemons.map(pokemon => {
         return (
           <div className="pokemon-info__img-wrapper">
